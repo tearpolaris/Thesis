7cdbabeddb62d52cb010b4e476b5599008b5f5f2
Anh Vo <minhanh427401@yahoo.com>
2019-10-28 20:27:27 +0700

    Add slide Connect_ioT and main.c

--- Project_STM32/Start_Project/main.c
+++ Project_STM32/Start_Project/main.c
@@ -11,21 +11,38 @@ int main(void) {
     //ESP8266_Str* ESP8266 = (ESP8266_Str*)calloc(1, sizeof(ESP8266_Str));
     ESP8266_Str ESP8266;
     ESP8266_Result ret;
-    TIM_TimeBaseInitTypeDef  TIM_TimeBase;
-    Init_TIM2(TIM_TimeBase);
-    TIM_Cmd(TIM2, ENABLE);
+    uint16_t humid, temp;
+    //TIM_TimeBaseInitTypeDef  TIM_TimeBase;
+    //Init_TIM2(TIM_TimeBase);
+    //TIM_Cmd(TIM2, ENABLE);
     Test_Interrupt();
-    Init_Debug_GPIO();
-    Debug_init_UART_Config(USART6, 9600);
-    //Transmit_UART(USART6, (uint8_t*)"Beautiful weather", strlen("Beautiful weather"));
+    //Init_Debug_GPIO();
+    //Debug_init_UART_Config(USART6, 9600);
+    //Transmit_string_UART(USART6, "Beautiful weather");
+    //ret = ESP8266_Init(&ESP8266, 115200);
+    //if (ret == ESP8266_OK) {
+    //    Transmit_UART(USART6, (uint8_t*)"\rInitialization ESP8266 successfully\n", (strlen)("Initialization ESP8266 successfully\n"));
+    //}
+    //else {
+    //    return -1;
+    //} 
+    //ESP8266_Setting_WebServer(&ESP8266, "D-Link_DIR-612", "nhatro2129", 80);
+    //Handle_Request_Browser(&ESP8266);    
+    //GPIO_SetBits(GPIOD, GPIO_Pin_12);
+    Initialization_General_DHT22();
+    DHT22_Get_Humid_Temp(&humid, &temp);
+    Init_General_Setting_YL69();
     ret = ESP8266_Init(&ESP8266, 115200);
     if (ret == ESP8266_OK) {
-        Transmit_UART(USART6, (uint8_t*)"\rInitialization ESP8266 successfully\n", (strlen)("Initialization ESP8266 successfully\n"));
+        //Transmit_UART(USART6, (uint8_t*)"\rInitialization ESP8266 successfully\n", (strlen)("Initialization ESP8266 successfully\n"));
+    }
+    else {
+        return -1;
     } 
     ESP8266_Setting_WebServer(&ESP8266, "D-Link_DIR-612", "nhatro2129", 80);
-    Handle_Request_Browser(&ESP8266);    
+    Handle_Request_Browser(&ESP8266);  
     while(1) {
     }
-		//return 0;
+		return 0;
 }
 
